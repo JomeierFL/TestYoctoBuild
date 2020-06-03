40d43f083163a2349cba3e356ca522cc67406f70
JomeierFL <57296434+JomeierFL@users.noreply.github.com>
2020-06-03 10:53:17 +0200

    Update failureAdjust.sh

--- failureAdjust.sh
+++ failureAdjust.sh
@@ -1,2 +1,1 @@
-sed -i 's#<h1>Build erfolgreich</h1>#<h1>Build Fehlgeschlagen, Konfiguration ok</h1>
-<p>Die Konfiguration war ok, aber der build hat nicht funktioniert</p>#g' response.html
+sed -i 's#<h1>Build erfolgreich</h1>#<h1>Build Fehlgeschlagen, Konfiguration ok</h1>\n<p>Die Konfiguration war ok, aber der build hat nicht funktioniert</p>#g' response.html
